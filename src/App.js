ff611cdeb5294eb87743868001709423d6e51204
natalia <nataliav@gmail.com>
2018-12-25 23:15:23 -0500

    Added hot reloading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { hot } from "react-hot-loader";
 import "./styles.css";
 
 class App extends Component {
@@ -9,7 +10,9 @@ class App extends Component {
     return (
       <div>
         <h1>Hello World and you!</h1>
-        <h2>Count: {this.state.count}</h2>
+        <h2 className={this.state.count > 10 ? "warning" : ""}>
+          Count: {this.state.count}
+        </h2>
         <button
           onClick={() => this.setState(state => ({ count: state.count + 1 }))}
         >
@@ -25,4 +28,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default hot(module)(App);
