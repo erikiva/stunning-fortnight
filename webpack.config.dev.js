e345d5cb84c843dbb09a9e168c466fd9d4f44588
natalia <nataliav@gmail.com>
2018-12-25 20:08:45 -0500

    Added support for clas properties with babel

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,5 +3,6 @@ const baseConfig = require("./webpack.config.base");
 
 module.exports = merge(baseConfig, {
   mode: "development",
-  devServer: { port: 9000 }
+  devServer: { port: 9000 },
+  devtool: "source-map"
 });
