54f099c30a9c6d5973c72c8b22eecba0c7df57de
natalia <nataliav@gmail.com>
2018-12-30 20:37:18 -0500

    Load component async with lazy and suspense

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React, { Component } from "react";
 import { hot } from "react-hot-loader";
 import "./styles.css";
 
+const Warning = React.lazy(() => import("./Warning"));
+
 class App extends Component {
   state = {
     count: 0
   };
   render() {
+    const { count } = this.state;
     return (
       <div>
         <h1>Hello World and you!</h1>
-        <h2 className={this.state.count > 10 ? "warning" : ""}>
-          Count: {this.state.count}
-        </h2>
+        <h2 className={count > 10 ? "warning" : ""}>Count: {count}</h2>
         <button
           onClick={() => this.setState(state => ({ count: state.count + 1 }))}
         >
@@ -23,6 +24,11 @@ class App extends Component {
         >
           Decrement
         </button>
+        {count > 10 ? (
+          <React.Suspense fallback={null}>
+            <Warning />
+          </React.Suspense>
+        ) : null}
       </div>
     );
   }
