c3776c0b9b7c5d4ed52b76995733def738d0e95a
natalia <nataliav@gmail.com>
2019-01-01 19:36:42 -0500

    Added React axe to review accesibility

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import DefaultErrorBoundary from './DefaultErrorBoundary'
 
+if (process.env.NODE_ENV === 'development') {
+  const axe = require('react-axe')
+  axe(React, ReactDOM, 1000)
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <DefaultErrorBoundary>
